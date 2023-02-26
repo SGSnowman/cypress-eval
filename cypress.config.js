a7523ef941b1308eb3d9b7d4f709763a87ed807e
Aditya Gaurav <adityaa.gaurav@gmail.com>
2023-02-26 22:34:54 +0530

    Book Store application tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,21 @@
 module.exports = {
   e2e: {
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    experimentalSkipDomainInjection : [
+      '*.criteo.com',
+      '*.googlesyndication.com/*'
+    ],
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'https://demoqa.com',
+    //End points
+    baseUrl: "https://demoqa.com",
+    env : {
+      //endpoints
+      bookStore:"/books/"
+    }
   },
-}
+};
